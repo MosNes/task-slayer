22c5926ec2ca05e7d878cbb75054e6096093a845
MosNes <7hekarl@gmail.com>
2022-05-16 22:01:31 -0400

    changed event listener to onsubmit instead of on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@
 //faster than using querySelector each time
 //use "El" at the end of the variable name to indicate it represents an HTML Element
 //use '#element-id' to access the HTML element by id
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //Event Handlers are functions that respond to an event, like a button click
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    //prevents the default behavior of the browser, such as refreshing on form submit
+    event.preventDefault();
+    console.log(event);
     //When you create an element, be sure to update its className so it can be
     //styled correctly by CSS
     var listItemEl = document.createElement("li");
@@ -18,7 +21,6 @@ var createTaskHandler = function() {
 
 //Pass in a callback function, like an Event Handler, as the 2nd argument
 //for addEventListener to make the event "do" something
-buttonEl.addEventListener("click", createTaskHandler);
-
+formEl.addEventListener("submit",createTaskHandler);
 
 
